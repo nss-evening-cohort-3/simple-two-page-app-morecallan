33a9e8a550163a713a54fda1b010489be0e3e5f4
Callan Morrison <callan.l.morrison@gmail.com>
2016-10-19 22:27:27 -0500

    completed

---
+++ Simple2Page/Simple2Page/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Simple2Page.Startup))]
+namespace Simple2Page
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
